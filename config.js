eedb51bad9dc4b559fe458a5fc637d1600f7ace1
DarkWinso <104155200+DarkWinso@users.noreply.github.com>
2023-03-21 12:46:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //============================================================( YOU CAN EDIT )====================================================================//
 
-global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE", //Put Open AI Api key
+global.openaikey = process.env.OPEN_AI_KEY || "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", //Put Open AI Api key
 	
-global.owner = process.env.OWNER || "94XXXXXXXX", //Bot Owner Number
+global.owner = process.env.OWNER || "94740842996", //Bot Owner Number
 	
 global.ownername = process.env.OWNER_NAME || "DarkWinzo", //Bot Owner Name
 	
@@ -14,11 +14,11 @@ global.open_ai = process.env.OPEN_AI || "false", //Open AI Bot : "true" = on/"fa
 	
 global.chatbot_pm = process.env.CHATBOT_PM || "true", //Inbox Msg Chat Bot : "true" = on/"false" = off
 	
-global.chatbot_group = process.env.OWNER || "true", //Group Msg Chat Bot : "true" = on/"false" = off
+global.chatbot_group = process.env.OWNER || "false", //Group Msg Chat Bot : "true" = on/"false" = off
 	
-global.language = process.env.LANGUAGE || "EN", //EN < = English , SI < = Sinhala
+global.language = process.env.LANGUAGE || "SI", //EN < = English , SI < = Sinhala
 	
-global.bot_present = process.env.PRESENT || "false" //false is recommand..because it make crash
+global.bot_present = process.env.PRESENT || "true" //false is recommand..because it make crash
 
 global.prefix = "." // Not need to change
 
